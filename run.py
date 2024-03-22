93fe8403ba1b15c355fe31506200e083a0634dd2
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2024-03-23 05:55:23 +0700

    Update run.py

--- run.py
+++ run.py
@@ -74,9 +74,9 @@ async def connect_to_wss(socks5_proxy, user_id):
 
 async def main():
     #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
-    _user_id = input('Enter your user ID: ')
+    _user_id = input('Please Enter your user ID: ')
     #put the proxy in a file in the format socks5://username:password@ip:port or socks5://ip:port
-    with open('proxy_list(for1).txt', 'r') as file:
+    with open('proxy_list.txt', 'r') as file:
         socks5_proxy_list = file.read().splitlines()
     
     tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in socks5_proxy_list]
